df21587f9c977253bdd273d43033248f93452aaf
Elias Keven <eliaskeven2@gmail.com>
2017-08-26 20:46:16 -0300

    inserindo procura por sabores

--- cafezinho-bom.js
+++ cafezinho-bom.js
@@ -1,20 +1,22 @@
 const listaDeCafe = []
 //acao(array):: string -> string -> numero -> obj
 const acao = adicionar => cafe => gosto => nivel => {
-	return adicionar.push({marca: cafe, sabor: gosto, nivelDoSabor: nivel}) 
+	return adicionar.push({marca: cafe,
+						  sabor: gosto,
+						  nivelDoSabor: nivel}) 
 }
  
 const adicionarCafes = acao(listaDeCafe)
 adicionarCafes('São Braz')('Bom')(6)
-adicionarCafes('Petinho')('Ruim')(8)
 adicionarCafes('Santa Clara')('Ótimo')(10)
 adicionarCafes('Melita')('Ótimo')(10)
-console.log(listaDeCafe)
+adicionarCafes('Petinho')('Ruim')(4)
 
-
-
-
-
+//acessarLista:: string -> array
+const acessarLista = procurarNaLista => saborInserido => 
+	procurarNaLista.filter(valor => valor.sabor === saborInserido) 
+const verificarSabor = acessarLista(listaDeCafe)
+console.log(verificarSabor('Ótimo'))
 
 
 
